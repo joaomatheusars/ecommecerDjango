a7a719463d51b35616de6e9756c6b1a7bd070df4
João Matheus <joaomatheusars@gmail.com>
2024-04-23 15:10:48 -0300

    feat: add authuser in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
