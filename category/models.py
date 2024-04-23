af2d31b839a4d9d826b2366c135db2bdab09b4a0
João Matheus <joaomatheusars@gmail.com>
2024-04-23 14:46:56 -0300

    fix: parameter 'upload' to 'upload_to'

--- category/models.py
+++ category/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.CharField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
-    cat_image = models.ImageField(upload='photo/categories', blank=True)
+    cat_image = models.ImageField(upload_to='photo/categories', blank=True)
 
     def __str__(self):
         return self.category_name
