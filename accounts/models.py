99e5880ffc03d18381c7800586ddd7efccc3f68c
João Matheus <joaomatheusars@gmail.com>
2024-04-23 15:09:30 -0300

    feat: add account manger

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,37 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email: raise ValueError("User must have an email address")
+        if not username: raise ValueError('User must have an username')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(
+            email= self.normalize_email(email),
+            username=username,
+            password= password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.is_admin=True
+        user.is_active=True
+        user.is_staff=True
+        user.is_superadmin=True
+        user.save(using=self.db)
+        return user
+
 class Account(AbstractBaseUser):
     first_name      = models.CharField(max_length=50)
     last_name       = models.CharField(max_length=50)
@@ -19,6 +50,8 @@ class Account(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
 
     def __str__(self):
         return self.email
