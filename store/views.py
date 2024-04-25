eafd3d28fb74ef72879b68a1eb8527dfca2e551f
João Matheus <joaomatheusars@gmail.com>
2024-04-25 12:35:16 -0300

    feat: creat a search function

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -43,3 +44,16 @@ def product_detail(request, category_slug, product_slug):
         'in_cart'       : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET.get('keyword')
+        
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword ) | Q(product_name__icontains=keyword)) # __icontains look for all description
+        
+    context = {
+        'products': products,
+        'products_count': products.count(),
+    }
+    return render(request, 'store/store.html', context)
