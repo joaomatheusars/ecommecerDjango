c741044a013227f749439abb43da290625f2c9a7
João Matheus <joaomatheusars@gmail.com>
2024-04-29 13:11:01 -0300

    fix: redirect page

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import requests.utils
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
@@ -6,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
-
+import requests
 
 # Verification email
 from django.contrib.sites.shortcuts import get_current_site
@@ -108,7 +109,16 @@ def login(request):
 
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials.')
             return redirect('login')
